97fb5e2ddd9fadb1fe2b03af2ecd7ba5ac93cb13
Rucha Nargunde <ruchanargunde@gmail.com>
2020-06-01 19:10:24 +0530

    views.py and models.py modified

--- covid/models.py
+++ covid/models.py
@@ -9,23 +9,23 @@ class requirement(db.Model):
     state = db.Column(db.String(100),nullable=False)
     email = db.Column(db.String(30),nullable=False)
     req = db.Column(db.Integer,nullable=False)
-    document_image = db.Column(db.String(64),nullable=False)
+    document_image = db.Column(db.String(64))
     contact = db.Column(db.String(10),nullable=False)    
     status = db.Column(db.String,default='orderplaced')
     cur_time = db.Column(db.DateTime,default=datetime.now())
     ngo_id = db.Column(db.Integer,db.ForeignKey('ngo.id'))
-    code = db.Column(db.String,nullable=False)
+    code = db.Column(db.String)
     ngo = db.relationship('ngo',backref='order',lazy=True)
-    def __init__(self,name=name,location=location,district=district,state=state,code=code,email=email,document_image=document_image,req=req,contact=contact):
+    def __init__(self,name=name,location=location,district=district,state=state,email=email,req=req,contact=contact):
         self.name = name
         self.location=location
         self.contact=contact
         self.district=district
         self.state=state
         self.email=email
-        self.document_image=document_image
+        # self.document_image=document_image
         self.req=req
-        self.code=code
+        # self.code=code
 
 class ngo(db.Model):
     __tablename__='ngo'
--- covid/requirements/views.py
+++ covid/requirements/views.py
@@ -1,4 +1,4 @@
-from flask import flash,render_template,redirect,url_for,request,Blueprint,abort
+from flask import flash,render_template,redirect,url_for,request,Blueprint,abort,jsonify,request
 from covid import db,mail,app
 from covid.models import requirement
 from covid.requirements.forms import Requirement
@@ -12,36 +12,53 @@ requirements = Blueprint('requirements',__name__)
 
 @requirements.route('/require',methods=['GET','POST'])
 def require():
-    form = Requirement()
-    if form.validate_on_submit():
+    # form = Requirement()
+    # if form.validate_on_submit():
         
-        name = form.name.data.lower()
-        location = form.location.data.lower()
-        district = form.district.data.lower()
-        state = form.state.data.lower()
-        req = form.requirements.data
-        email = form.email.data
-        code = ''.join(random.choices(string.ascii_lowercase,k=20))
-        f = send_qr(code,email)
-        print("1")
-        msg = Message('Here is your QRCode',recipients=[email])
-        print("2")
-        with app.open_resource('requirements\\qrcodes\\'+f) as fi:
-            msg.attach(f,'image/png',fi.read())
-        print("3")
-        mail.send(msg)
-        print("4")
-        document_image = add_image(form.doc.data,name)
-        contact = form.contact.data
-        req = requirement(name=name,location=location,email=email,district=district,state=state,code=code,req=req,document_image=document_image,contact=contact)
-        db.session.add(req)
-        db.session.commit()
-        print(req.name)
-        flash('Requirement submitted successfully!')
-        return redirect(url_for('requirements.reqlist'))
-    return render_template('requests.html',form=form)
+    #     name = form.name.data.lower()
+    #     location = form.location.data.lower()
+    #     district = form.district.data.lower()
+    #     state = form.state.data.lower()
+    #     req = form.requirements.data
+    #     email = form.email.data
+    #     code = ''.join(random.choices(string.ascii_lowercase,k=20))
+
+    requirement_data=request.get_json()
+
+    new_requirement=requirement(name=requirement_data['name'],location=requirement_data['location'],
+                                    district=requirement_data['district'],state=requirement_data['State'],
+                                    email=requirement_data['email'],contact=requirement_data['contact'],
+                                    req=requirement_data['requirements'])
+
+        # f = send_qr(code,email)
+        # print("1")
+        # msg = Message('Here is your QRCode',recipients=[email])
+        # print("2")
+        # with app.open_resource('requirements\\qrcodes\\'+f) as fi:
+        #     msg.attach(f,'image/png',fi.read())
+        # print("3")
+        # mail.send(msg)
+        # print("4")
+        # document_image = add_image(form.doc.data,name)
+        # contact = form.contact.data
+        # req = requirement(name=name,location=location,email=email,district=district,state=state,code=code,req=req,document_image=document_image,contact=contact)
+    db.session.add(new_requirement)
+    db.session.commit()
+
+    return "Done"
+
+    #     print(req.name)
+    #     flash('Requirement submitted successfully!')
+    #     return redirect(url_for('requirements.reqlist'))
+    # return render_template('requests.html',form=form)
 
 @requirements.route('/reqlist')
 def reqlist():
     reqs = requirement.query.order_by(requirement.cur_time.desc())
-    return render_template('requests_list.html',requests=reqs)
+    
+    requests=[]
+
+    for request in reqs:
+        requests.append({'Name': request.name ,'Location': request.location+","+request.district+","+request.state , 'Requirement': request.req ,'ContactNo': request.contact})
+
+    return jsonify({'requests': requests})
